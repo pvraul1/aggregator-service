979350c061e189422600bf5b131b1f40718f631c
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-06-08 16:06:07 +0200

    172. [Aggregator] - Hot Price Stream

--- src/main/kotlin/com/rperez365/aggregator/client/StockServiceClient.kt
+++ src/main/kotlin/com/rperez365/aggregator/client/StockServiceClient.kt
@@ -8,12 +8,17 @@ import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.util.retry.Retry
+import java.time.Duration
+import java.util.*
 
 class StockServiceClient(
     private val client: WebClient
 ) {
 
     companion object : KLogging()
+
+    private lateinit var flux: Flux<PriceUpdate>
 
     fun getStockPrice(ticker: Ticker): Mono<StockPriceResponse> {
         return client.get()
@@ -22,12 +27,27 @@ class StockServiceClient(
             .bodyToMono(StockPriceResponse::class.java)
     }
 
-    fun getPriceUpdates(): Flux<PriceUpdate> {
+    fun priceUpdatesStream(): Flux<PriceUpdate> {
+        if (Objects.isNull(this.flux)) {
+            this.flux = this.getPriceUpdates()
+        }
+
+        return this.flux
+    }
+
+    private fun getPriceUpdates(): Flux<PriceUpdate> {
         return client.get()
             .uri("/stock/price-stream")
             .accept(MediaType.APPLICATION_NDJSON)
             .retrieve()
             .bodyToFlux(PriceUpdate::class.java)
+            .retryWhen(retry())
+            .cache(1)
+    }
+
+    private fun retry(): Retry {
+        return Retry.fixedDelay(100, Duration.ofSeconds(1))
+            .doBeforeRetry { retryRetrySignal -> logger.error("Stock service price steam call failed. Retrying: ${retryRetrySignal.failure().message}") }
     }
 
 }
